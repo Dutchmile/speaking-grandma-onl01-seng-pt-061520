34503c5f50c9f5925739785b0239a589db78a985
Christopher Dutcher <cadutcher@icloud.com>
2020-06-19 00:38:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ speak_to_grandma = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(greeting)
 if greeting == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif greeting == greeting.upcase != "I LOVE YOU GRANDMA!"
+elsif greeting == greeting.upcase
   return "NO, NOT SINCE 1938!"
 else
   return "HUH?! SPEAK UP, SONNY!"
