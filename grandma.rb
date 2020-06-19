87385a70949ec3d29cca2df1f0e7299eceaac9bc
Christopher Dutcher <cadutcher@icloud.com>
2020-06-19 00:34:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,6 +9,7 @@ elsif greeting == "WHAT DID YOU EAT TODAY?"
 else
   puts "HUH?! SPEAK UP, SONNY!"
 end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
